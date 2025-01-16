c16fa8ee880d7a38ee3b6aa469d46d905d3b706d
Ryan Hieber <floatingpoint@gmail.com>
2025-01-15 22:27:43 -0800

    Add Tab and window keybinds and a couple other things

--- lua/rhieber/remap.lua
+++ lua/rhieber/remap.lua
@@ -1,3 +1,21 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+local keymap = vim.keymap
+
+keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
+
+-- window management
+--keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" })
+--keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" })
+--keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" })
+--keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" })
+
+keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "New tab" })
+keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" })
+keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Next tab" })
+keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Previous tab" })
+keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab"})
+
 
--- lua/rhieber/set.lua
+++ lua/rhieber/set.lua
@@ -12,9 +12,14 @@ vim.opt.expandtab = true
 
 vim.opt.smartindent = true
 
+-- search settings
+vim.opt.ignorecase = true
+vim.opt.smartcase = true -- if you include mixed case in your search, assumes you want case sensitive.
+
 -- no linewrapping
 vim.opt.wrap = false
 
+vim.opt.backspace = "indent,eol,start" -- allow backspace on indent, eol, or insert mode start position
 
 -- No default vim backups. instead, have undotree save to file so you undo history persists.
 vim.opt.swapfile = false
@@ -23,7 +28,7 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 -- Don't highlight search terms
-vim.opt.hlsearch = false
+ vim.opt.hlsearch = true 
 
 -- Enable Incremental Search
 vim.opt.incsearch = true
@@ -40,9 +45,9 @@ vim.opt.updatetime = 50
 -- Add a guid column at 80 chars 
 vim.opt.colorcolumn = "80"
 
--- Other stuff Primeagen had that wasn't explained
--- vim.opt.signcolumn = "yes"
--- vim.opt.isfname:append("@-@")
+-- show sign column so that text doesn't shift. 
+vim.opt.signcolumn = "yes"
 
-
+-- clipboard
+vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register.
 
