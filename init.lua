7d60ee066a69ab293c2e6d994667f2d2cec048dd
Ryan Hieber <floatingpoint@gmail.com>
2025-01-17 01:46:49 -0800

    Remove redundant lazy setup in init

--- init.lua
+++ init.lua
@@ -3,11 +3,9 @@ vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
 -- Point neovim to the python virtualenv to remove python provider warnings.
-local home = os.getenv('HOME')
+local home = os.getenv("HOME")
 --vim.env.PYENV_VERSION = vim.fn.system('pyenv version'):match('(%S+)%s+%(.-%)')
 vim.g.python3_host_prog = home .. "/.virtualenvs/neovim/bin/python3"
 
 require("config.lazy")
-require("lazy").setup("plugins")
 require("rhieber")
-
