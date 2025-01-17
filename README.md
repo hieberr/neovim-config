165e45cece145583c2a9097daba999731ea1cbdb
Ryan Hieber <floatingpoint@gmail.com>
2025-01-16 23:22:15 -0800

    Fixing lazyvim

--- README.md
+++ README.md
@@ -6,12 +6,27 @@ My ~/.config/nvim directory
 # Installation
 
 - Clone this repo into ~/.config and rename to nvim
-- Install [Packer](https://github.com/wbthomason/packer.nvim)
-- In nvim navigate to lua/rhieber/packer.lua and source it (:so)
-- Run :PackerSync to syncronize all the other plugins
-- If node isn't installed install it (for tree sitter to work properly. Otherwise you will get errors that node isn't installed whenever it tries to build a language that requires it)a
+- Uses LazyVim plugin manager. Should bootstrap itself when your run nvim the first time.
+- Run :checkhealth lazy to verify it is installed correctly
+- Run :Lazy and do a shift-S to sync everything
+- Install a nerd font if you see glyphs not rendering. I like "jet brains mono nerd font"
+- If node isn't installed install it (for tree sitter to work properly. Otherwise you will get errors that node isn't installed whenever it tries to build a language that requires it)
 - Install ripgrep for grep search of files. ```brew install ripgrep```
+- Install fd ```brew install fd```. Not required but used by telescope for better file searching.
+- Setup python. Not sure if this is needed but otherwise neovim has a warning.
+   ```
+    brew install pyenv
+    brew install virtualenv
 
+    mkdir ~/.virtualenvs
+    virtualenv ~/.virtualenvs/neovim
+
+    cd ~/.virtualenvs/neovim
+    source bin/activate
+    pip3 install pynvim
+  ```
+
+    
 
 
 
--- lua/rhieber/set.lua
+++ lua/rhieber/set.lua
@@ -6,7 +6,7 @@
 vim.opt.nu = true
 -- Enable relative line numbers
 vim.opt.relativenumber = true
--- show sign column so that text doesn't shift. 
+-- show sign column so that text doesn't shift.
 vim.opt.signcolumn = "yes"
 
 -- Tabspacing/indent
@@ -19,16 +19,16 @@ vim.opt.smartindent = true
 -- Ignore case when searching
 vim.opt.ignorecase = true
 -- If you include mixed case in your search, assumes you want case sensitive.
-vim.opt.smartcase = true 
+vim.opt.smartcase = true
 -- Don't highlight search terms
- vim.opt.hlsearch = true 
+vim.opt.hlsearch = true
 -- Enable Incremental Search
 vim.opt.incsearch = true
 
 -- No linewrapping
 vim.opt.wrap = false
 -- Allow backspace on indent, eol, or insert mode start position
-vim.opt.backspace = "indent,eol,start" 
+vim.opt.backspace = "indent,eol,start"
 
 -- No default vim backups. instead, have undotree save to file so you undo history persists.
 vim.opt.swapfile = false
@@ -36,18 +36,17 @@ vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
--- Not sure what this does. 
+-- Use 24 bit colors
 vim.opt.termguicolors = true
 
 -- Always have at least 8 characters at the end of a file??
-vim.opt.scrolloff = 8 
+vim.opt.scrolloff = 8
 
 -- Fast update time
 vim.opt.updatetime = 50
 
--- Add a guid column at 100 chars 
+-- Add a guid column at 100 chars
 vim.opt.colorcolumn = "100"
 
 -- clipboard
 vim.opt.clipboard:append("unnamedplus") -- use system clipboard as default register.
-
