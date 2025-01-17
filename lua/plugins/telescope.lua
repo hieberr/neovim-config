b9bbef57e944628b8245a60cef69f31e8a373c1c
Ryan Hieber <floatingpoint@gmail.com>
2025-01-17 02:18:42 -0800

    Change telescope keymapping

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -7,13 +7,18 @@ return {
 		tag = "0.1.8",
 		-- or                            , branch = '0.1.x',
 		dependencies = { { "nvim-lua/plenary.nvim" } },
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>pf", builtin.find_files, { desc = "Telescope find files" })
-			vim.keymap.set("n", "<C-p>", builtin.git_files, { desc = "Telescope find files" })
-			vim.keymap.set("n", "<leader>ps", function()
-				builtin.grep_string({ search = vim.fn.input("Grep > ") })
-			end)
-		end,
+		config = function() end,
+		keys = {
+			{ "<leader>pf", "<cmd>Telescope find_files<cr>", desc = "Telescope find files" },
+			{
+				"<leader>ps",
+				function()
+					local builtin = require("telescope.builtin")
+					builtin.grep_string({ search = vim.fn.input("Grep > ") })
+				end,
+				desc = "Telescope grep string",
+			},
+		},
 	},
+	--require("lazy").setup("plugins")
 }
